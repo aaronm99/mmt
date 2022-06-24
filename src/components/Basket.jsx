acb3334cf0be67e94030332d4bbb97b5501fbb8f
Aaron Mistry <aaron.rohin@hotmail.co.uk>
2022-06-24 17:15:12 +0100

    changed layout and added input boxes

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,30 +1,43 @@
 import React from "react";
+import { MdClose } from "react-icons/md";
+import { useForm } from "@mantine/form";
+import { NumberInput } from "@mantine/core";
 
 const Basket = () => {
   return (
     <div className="basket__body">
-      <div className="item">
-        <div className="item__name">Mountain Dew</div>
-        <div className="item__quantity">2</div>
-        <div className="item__price">$3.60</div>
-        <div className="item__delete">x</div>
-      </div>
-      <div className="item">
-        <div className="item__name">Desperados</div>
-        <div className="item__quantity">6</div>
-        <div className="item__price">$15.50</div>
-        <div className="item__delete">x</div>
-      </div>
-      <div className="item">
-        <div className="item__name">Jack Daniels</div>
-        <div className="item__quantity">4</div>
-        <div className="item__price">$13.40</div>
-        <div className="item__delete">x</div>
+      <div className="basket__items">
+        <div className="item">
+          <div className="item__name">Mountain Dew</div>
+          <NumberInput mt="sm" className="item__quantity" defaultValue={2} />
+          <div className="item__price">$3.60</div>
+          <div className="item__delete">
+            <MdClose />
+          </div>
+        </div>
+        <div className="item">
+          <div className="item__name">Desperados</div>
+          <NumberInput mt="sm" className="item__quantity" defaultValue={6} />
+          <div className="item__price">$15.50</div>
+          <div className="item__delete">
+            <MdClose />
+          </div>
+        </div>
+        <div className="item">
+          <div className="item__name">Jack Daniels</div>
+          <NumberInput mt="sm" className="item__quantity" defaultValue={4} />
+          <div className="item__price">$13.40</div>
+          <div className="item__delete">
+            <MdClose />
+          </div>
+        </div>
       </div>
       <div className="basket__checkout">
         <div className="total__price">$32.50</div>
-        <div className="clear__btn">Clear</div>
-        <div className="checkout__btn">Check Out {">"}</div>
+        <div className="checkout__buttons">
+          <button className="clear__btn">Clear</button>
+          <button className="checkout__btn">Check Out {">"}</button>
+        </div>
       </div>
     </div>
   );
